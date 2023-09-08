ac94088ca3b60243c01c27ade0fc0e87e4792894
perealvees <perealvees@gmail.com>
2023-09-07 21:34:08 -0300

    ajuste de pasta

--- 01_mensagem/app.js
+++
@@ -1,6 +0,0 @@
-// importar arquivo de calculo//
-var calculos = require('./calculos');
-//obtem a função para multiplicar
-var multiplicar = calculos.multiplicar;
-//realiza o calculo
-console.log(multiplicar, (5,6));
